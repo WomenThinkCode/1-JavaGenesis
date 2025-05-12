f97db7ac00626056c16ff9ca4726252bb2d55742
Khosi Dlomo <makhosijdlomo97@gmail.com>
2025-05-12 20:27:12 +0200

    First application exercise

---
+++ src/main/java/org/example/First.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import javax.swing.JOptionPane;
+
+public class First {
+    public static void main(String[] args){
+        System.out.println("My first Java application");
+        JOptionPane.showMessageDialog(null, "My first Java application");
+    }
+}
---
+++ src/main/java/org/exercises/Hello2.java
@@ -0,0 +1,9 @@
+package org.exercises;
+
+public class Hello2 {
+    //This is a method header that returns nothing.
+    public static void main(String[] args){
+        //This is a method body that prints a message to the console.
+        System.out.println("Hello World.");
+    }
+}

a09ee488b7adaa86b4dcb99aabb3d71ae9cf1a86
ammohlojhb024 <ammohlojhb024@student.wethinkcode.co.za>
2025-05-16 09:49:40 +0200

    Added the songlyrics class

---
+++ src/main/java/org/example/SongLyrics.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class SongLyrics {
+    public static void main(String[] args) {
+        System.out.println("The Weeknd - After Hours");
+        System.out.println("Cause my heart belongs to you");
+        System.out.println("I'll risk it all for you");
+        System.out.println("I want you next to me");
+        System.out.println("This time, I'll never leave");
+    }
+}

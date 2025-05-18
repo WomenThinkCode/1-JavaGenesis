b032333792269b384c23b638963458e1e318b40a
MaryMdukaza <marymdukaza@gmail.com>
2025-05-18 22:24:13 +0200

    completed Triangle

---
+++ src/main/java/org/example/Triangle.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Triangle {
+    public static void main(String[] args) {
+        System.out.println("""
+                      T \s
+                     TTT \s
+                    TTTTT \s
+                   TTTTTTT \s
+                  TTTTTTTTT \s
+                 TTTTTTTTTTT \s
+                TTTTTTTTTTTTT \s""");
+    }
+}

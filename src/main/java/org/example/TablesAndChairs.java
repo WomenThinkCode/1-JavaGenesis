a634923ff1aa803cf6bc71f38e093b9dd4f5641e
MaryMdukaza <marymdukaza@gmail.com>
2025-05-18 22:22:49 +0200

    completed TablesAndChairs

---
+++ src/main/java/org/example/TablesAndChairs.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class TablesAndChairs {
+    public static void main(String[] args) {
+        System.out.println("""
+                x                      X \s
+                x                      x \s
+                x      xxxxxxxxxx      x \s
+                xxxxx  x        x  xxxxx \s
+                x   x  x        x  x   x \s
+                x   x  x        x  x   x \s""");
+    }
+}

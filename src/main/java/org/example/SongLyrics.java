502cf2c4fa15dd2ef55e596a04be0c460cd48ffd
MaryMdukaza <marymdukaza@gmail.com>
2025-05-18 22:20:20 +0200

    completed SongLyrics

---
+++ src/main/java/org/example/SongLyrics.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class SongLyrics {
+    public static void main(String[] args) {
+        System.out.println("Can");
+        System.out.println("You");
+        System.out.println("Feel");
+        System.out.println("My");
+        System.out.print("Heart");
+    }
+}
